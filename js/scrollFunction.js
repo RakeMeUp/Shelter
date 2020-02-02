e841d89e9d93e28bcbe525d828283fdf07ca868d
Rake <bencehetes@gmail.com>
2020-02-02 12:23:02 +0100

    New Design

--- js/scrollFunction.js
+++ js/scrollFunction.js
@@ -3,32 +3,7 @@ window.onscroll = function() {scrollFunction()};
 
       
       function scrollFunction() {
-        if (document.documentElement.scrollTop > 0) {
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-
-                document.getElementById("header").style.fontSize = "0.7em";
-                document.getElementById("TheHeader").style.paddingBottom = "0";
-                document.getElementById("TheHeader").style.zIndex = 1;}
-              else {
-                document.getElementById("header").style.fontSize = "0.5em";
-                document.getElementById("TheHeader").style.paddingBottom = "0";
-                document.getElementById("TheHeader").style.zIndex = 1;
-              }
-            }
-        else {
-            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          
-              document.getElementById("header").style.fontSize = "1em";
-              document.getElementById("TheHeader").style.paddingBottom = "0.5em";
-              document.getElementById("TheHeader").style.zIndex = 0;}
-            else {
-              document.getElementById("header").style.fontSize = "1em";
-              document.getElementById("TheHeader").style.paddingBottom = "0.5em";
-              document.getElementById("TheHeader").style.zIndex = 0;
-            }
-        }
-
-        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
           document.getElementById("topBtn").style.display = "block";
         } else {
           document.getElementById("topBtn").style.display = "none";
